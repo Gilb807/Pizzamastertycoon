93c51322dad0db8d591e47a0e2688320a4636137
Gilb807 <gilbertomsouza29@gmail.com>
2025-07-10 20:54:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+let clbc = 0;
+let xp = 0;
+
+function venderPizza() {
+  clbc += 10;
+  xp += 5;
+  document.getElementById("saldo").innerText = clbc;
+  document.getElementById("xp").innerText = xp;
+}
---
+++ supabase.js
@@ -0,0 +1,7 @@
+
+// Simulação de login com Google
+document.getElementById('login-btn').addEventListener('click', () => {
+  document.getElementById('login').style.display = 'none';
+  document.getElementById('game').style.display = 'block';
+  document.getElementById('username').innerText = 'Gilberto';
+});
